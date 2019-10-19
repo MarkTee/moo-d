fdac2aa2fcde0c82f3dbd2245eb0f00a22c65d98
Alex Rostron <alex.rostron24@gmail.com>
2019-10-19 17:20:47 -0600

    Add custom ArrayAdapter based off listycity

---
+++ code/app/src/main/java/com/gittfo/moodtracker/MoodHistoryAdapter.java
@@ -0,0 +1,47 @@
+package com.gittfo.moodtracker;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+
+
+
+import java.util.ArrayList;
+
+public class MoodHistoryAdapter extends ArrayAdapter {
+    private ArrayList<MoodEvent> moodHistory; // should this be a list or a moodhistory?
+    private Context context;
+
+
+    public MoodHistoryAdapter(Context context, MoodHistory moodHistory){
+        super(context,0, moodHistory.getMoodEvents());
+        this.moodHistory = moodHistory.getMoodEvents();
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View view = convertView;
+        if(view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.content, parent,false);
+        }
+
+        MoodEvent moodE = moodHistory.get(position);
+        TextView thing1 = view.findViewById(R.id.thing1);
+        TextView thing2 = view.findViewById(R.id.thing2);
+        thing1.setText(moodE.getComment());
+        thing2.setText(moodE.getReason());
+
+        return view;
+    }
+
+
+
+}
