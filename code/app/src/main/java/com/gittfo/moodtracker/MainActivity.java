d9eda840107ad25d8a858a29dd0346eb697ad2b8
Alex Rostron <alex.rostron24@gmail.com>
2019-10-19 18:17:01 -0600

    Add example mood history

--- code/app/src/main/java/com/gittfo/moodtracker/MainActivity.java
+++ code/app/src/main/java/com/gittfo/moodtracker/MainActivity.java
@@ -1,13 +1,63 @@
 package com.gittfo.moodtracker;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        exampleListView();
+    }
+
+    /**
+     * Setup a quick example 
+     */
+    private void exampleListView(){
+        Mood mood = new Mood(new Color(),
+                "emoticon string",
+                Mood.EmotionalState.HAPPY,
+                420);
+
+        MoodEvent moodEvent = new MoodEvent("comment",
+                "location",
+                "photoref",
+                "reason",
+                new Date(),
+                0,
+                mood);
+
+        //
+        ArrayList<MoodEvent> moodEventList = new ArrayList<>();
+        MoodHistory moodHistory = new MoodHistory(moodEventList);
+
+        //Populate list
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+        moodEventList.add(moodEvent);
+
+        //Create listViewAdapter and link it to listView
+        MoodHistoryAdapter mhAdapter = new MoodHistoryAdapter(this, moodHistory);
+        ListView historyView = findViewById(R.id.mood_history);
+        historyView.setAdapter(mhAdapter);
     }
 }
