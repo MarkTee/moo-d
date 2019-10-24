efbb3a779ff2d23aa10669ec78733d58cbc00c6a
Mark Thomas <MarkTee@users.noreply.github.com>
2019-10-23 18:55:12 -0600

    Implement ViewPager
    Using this ViewPager, the user will be able to swipe between entry
    screens.

--- code/app/src/main/java/com/gittfo/moodtracker/AddMoodEventActivity.java
+++ code/app/src/main/java/com/gittfo/moodtracker/AddMoodEventActivity.java
@@ -1,13 +1,48 @@
 package com.gittfo.moodtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 
 public class AddMoodEventActivity extends AppCompatActivity {
+    private static final int NUM_PAGES = 3; // Mood Event entry has 3 separate pages
+
+    private ViewPager mPager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_mood_event);
+
+        // Instantiate a ViewPager and a PagerAdapter
+        mPager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new AddMoodEventPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(pagerAdapter);
+    }
+
+    private class AddMoodEventPagerAdapter extends FragmentStatePagerAdapter {
+        public AddMoodEventPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            AddMoodEventPageFragment f = new AddMoodEventPageFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            f.setArguments(args);
+            return f;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
