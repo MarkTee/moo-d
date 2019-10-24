b0aaca1d6c1fa582633434f19b10ccee70cc886d
Mark Thomas <MarkTee@users.noreply.github.com>
2019-10-23 18:46:59 -0600

    Add PageFragment class
    These fragments are used in AddMoodEventActivity's ViewPager

---
+++ code/app/src/main/java/com/gittfo/moodtracker/AddMoodEventPageFragment.java
@@ -0,0 +1,39 @@
+package com.gittfo.moodtracker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class AddMoodEventPageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        int position = this.getArguments().getInt("position");
+
+        int currentPage = R.layout.mood_entry_screen_1;
+        switch (position) {
+            case 0:
+                currentPage = R.layout.mood_entry_screen_1;
+                break;
+
+            case 1:
+                currentPage = R.layout.mood_entry_screen_2;
+                break;
+
+            case 2:
+                currentPage = R.layout.mood_entry_screen_3;
+                break;
+        }
+
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                currentPage, container, false);
+
+        return rootView;
+    }
+}
