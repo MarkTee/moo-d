0807bdcecf85db1f829093271502148f0ef0807f
Alex Rostron <alex.rostron24@gmail.com>
2019-10-25 16:31:25 -0600

    Add a basic RecyclerView

--- code/app/src/main/java/com/gittfo/moodtracker/views/moodhistory/MoodHistoryTest.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/moodhistory/MoodHistoryTest.java
@@ -1,6 +1,8 @@
 package com.gittfo.moodtracker.views.moodhistory;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ListView;
@@ -14,8 +16,25 @@ public class MoodHistoryTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_history_test);
 
-        ListView mood_history_view = findViewById(R.id.mood_history);
-        MoodHistory moodHistory = new MoodHistory();
-        moodHistory.render(this, mood_history_view);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.mood_history);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = {"first one", "second one"};
+        MyAdapter mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
+
+        //ListView mood_history_view = findViewById(R.id.mood_history);
+        //MoodHistory moodHistory = new MoodHistory();
+        //moodHistory.render(this, mood_history_view);
     }
 }
