30d8a7b2153fe6f5050c0918b2aa68e23da7cb23
Jacob Reckhard <jacobreckhard@gmail.com>
2019-10-25 16:58:27 -0600

    added plus button

--- code/app/src/main/java/com/gittfo/moodtracker/views/MainActivity.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/MainActivity.java
@@ -1,7 +1,12 @@
 package com.gittfo.moodtracker.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.gittfo.moodtracker.views.addmood.AddMoodEventActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void createMoodEvent(View view) {
+        Intent i = new Intent(this, AddMoodEventActivity.class);
+        this.startActivity(i);
+    }
 }
