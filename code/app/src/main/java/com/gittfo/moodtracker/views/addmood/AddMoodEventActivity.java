c447d58755e1063b5cb6057e982de1efa57d7130
Mark Thomas <MarkTee@users.noreply.github.com>
2019-10-29 19:06:05 -0600

    Remove references to ViewPager
    Our Mood Entry screen has been simplified to a single screen of input,
    so we no longer need to use a ViewPager

--- code/app/src/main/java/com/gittfo/moodtracker/views/addmood/AddMoodEventActivity.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/addmood/AddMoodEventActivity.java
@@ -2,21 +2,12 @@ package com.gittfo.moodtracker.views.addmood;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.gittfo.moodtracker.views.R;
 
 public class AddMoodEventActivity extends AppCompatActivity {
-    private static final int NUM_PAGES = 3; // Mood Event entry has 3 separate pages
-
-    private ViewPager mPager;
-    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +16,7 @@ public class AddMoodEventActivity extends AppCompatActivity {
         // Hide ActionBar so that Mood Entry is full-screen
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-
         setContentView(R.layout.activity_add_mood_event);
 
-        // Instantiate a ViewPager and a PagerAdapter
-        mPager = (ViewPager) findViewById(R.id.pager);
-        pagerAdapter = new AddMoodEventPagerAdapter(getSupportFragmentManager());
-        mPager.setAdapter(pagerAdapter);
-    }
-
-    private class AddMoodEventPagerAdapter extends FragmentStatePagerAdapter {
-        public AddMoodEventPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            AddMoodEventPageFragment f = new AddMoodEventPageFragment();
-            Bundle args = new Bundle();
-            args.putInt("position", position);
-            f.setArguments(args);
-            return f;
-        }
-
-        @Override
-        public int getCount() {
-            return NUM_PAGES;
-        }
     }
 }
