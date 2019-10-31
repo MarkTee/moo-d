3c44275747867faea267b14f436e89a20282aba5
Mark Thomas <MarkTee@users.noreply.github.com>
2019-10-30 18:04:33 -0600

    Obtain and display current date to user

--- code/app/src/main/java/com/gittfo/moodtracker/views/addmood/AddMoodEventActivity.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/addmood/AddMoodEventActivity.java
@@ -4,10 +4,30 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.gittfo.moodtracker.views.R;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class AddMoodEventActivity extends AppCompatActivity {
+
+    private TextView dateDisplay;
+    private TextView timeDisplay;
+
+    // Get the current date and time, which are used to create a Mood Event
+    Date currentDate = new Date();
+
+    // Format date for display
+    Format dateFormat = new SimpleDateFormat("MMMM dd, yyyy");
+    String mDate = dateFormat.format(currentDate);
+
+    // Format time for display
+    Format timeFormat = new SimpleDateFormat("h:mm a");
+    String mTime = timeFormat.format(currentDate);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +37,13 @@ public class AddMoodEventActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_add_mood_event);
+
+        dateDisplay = findViewById(R.id.date_display);
+        dateDisplay.setText(mDate);
+
+        timeDisplay = findViewById(R.id.time_display);
+        timeDisplay.setText(mTime);
+
 
     }
 }
