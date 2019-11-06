585144ab2b18b5a7ff112100f6ec062b69d07723
Mark Thomas <MarkTee@users.noreply.github.com>
2019-11-05 20:41:44 -0700

    Add tests for all other default moods

--- code/app/src/test/java/com/gittfo/moodtracker/MoodTest.java
+++ code/app/src/test/java/com/gittfo/moodtracker/MoodTest.java
@@ -31,4 +31,74 @@ public class MoodTest {
         Mood mood = Mood.moodFromEmotionalState(emotionalState);
         assertEquals(mood, Mood.DEFAULT_HAPPY);
     }
+
+    // Test the "SAD" mood
+    @Test
+    public void testSad() {
+        String moodString = "SAD";
+
+        // Given a string, obtain the correct emotional state
+        Mood.EmotionalState emotionalState = emotionalStateFromString(moodString);
+        assertEquals(emotionalState, Mood.EmotionalState.SAD);
+
+        // Given an emotional state, obtain the correct default mood
+        Mood mood = Mood.moodFromEmotionalState(emotionalState);
+        assertEquals(mood, Mood.DEFAULT_SAD);
+    }
+
+    // Test the "SURPRISED" mood
+    @Test
+    public void testSurprised() {
+        String moodString = "SURPRISED";
+
+        // Given a string, obtain the correct emotional state
+        Mood.EmotionalState emotionalState = emotionalStateFromString(moodString);
+        assertEquals(emotionalState, Mood.EmotionalState.SURPRISED);
+
+        // Given an emotional state, obtain the correct default mood
+        Mood mood = Mood.moodFromEmotionalState(emotionalState);
+        assertEquals(mood, Mood.DEFAULT_SURPRISED);
+    }
+
+    // Test the "AFRAID" mood
+    @Test
+    public void testAfraid() {
+        String moodString = "AFRAID";
+
+        // Given a string, obtain the correct emotional state
+        Mood.EmotionalState emotionalState = emotionalStateFromString(moodString);
+        assertEquals(emotionalState, Mood.EmotionalState.AFRAID);
+
+        // Given an emotional state, obtain the correct default mood
+        Mood mood = Mood.moodFromEmotionalState(emotionalState);
+        assertEquals(mood, Mood.DEFAULT_AFRAID);
+    }
+
+    // Test the "DISGUSTED" mood
+    @Test
+    public void testDisgusted() {
+        String moodString = "DISGUSTED";
+
+        // Given a string, obtain the correct emotional state
+        Mood.EmotionalState emotionalState = emotionalStateFromString(moodString);
+        assertEquals(emotionalState, Mood.EmotionalState.DISGUSTED);
+
+        // Given an emotional state, obtain the correct default mood
+        Mood mood = Mood.moodFromEmotionalState(emotionalState);
+        assertEquals(mood, Mood.DEFAULT_DISGUSTED);
+    }
+
+    // Test the "ANGRY" mood
+    @Test
+    public void testAngry() {
+        String moodString = "ANGRY";
+
+        // Given a string, obtain the correct emotional state
+        Mood.EmotionalState emotionalState = emotionalStateFromString(moodString);
+        assertEquals(emotionalState, Mood.EmotionalState.ANGRY);
+
+        // Given an emotional state, obtain the correct default mood
+        Mood mood = Mood.moodFromEmotionalState(emotionalState);
+        assertEquals(mood, Mood.DEFAULT_ANGRY);
+    }
 }
