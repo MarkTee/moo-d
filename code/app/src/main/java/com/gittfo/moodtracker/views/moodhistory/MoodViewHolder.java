91508bbdf2617e1ce1ffb028bcc32106f8165a3d
Chuck H <cthanson@ualberta.ca>
2019-11-07 15:45:32 -0700

    Mood text is now colored according to mood choice

--- code/app/src/main/java/com/gittfo/moodtracker/views/moodhistory/MoodViewHolder.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/moodhistory/MoodViewHolder.java
@@ -4,6 +4,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.gittfo.moodtracker.mood.MoodEvent;
@@ -58,24 +59,32 @@ public class MoodViewHolder extends RecyclerView.ViewHolder {
         eventSocialSituation.setText(moodEvent.getSocialSituation().toString().toLowerCase());
 
         ImageView eventIcon = container.findViewById(R.id.user_emotion_imageView);
+        TextView eventColor = container.findViewById(R.id.user_mood_textView);
+
         switch  (moodEvent.getMood()){
             case SAD:
                 eventIcon.setImageResource(R.drawable.ic_sad_icon_24dp);
+                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonSad));
                 break;
             case ANGRY:
                 eventIcon.setImageResource(R.drawable.ic_angry_icon_24dp);
+                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonAngry));
                 break;
             case HAPPY:
                 eventIcon.setImageResource(R.drawable.ic_happy_icon_24dp);
+                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonHappy));
                 break;
             case AFRAID:
                 eventIcon.setImageResource(R.drawable.ic_afraid_icon_24dp);
+                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonAfraid));
                 break;
             case DISGUSTED:
                 eventIcon.setImageResource(R.drawable.ic_disgusted_icon_24dp);
+                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonDisgusted));
                 break;
             case SURPRISED:
                 eventIcon.setImageResource(R.drawable.ic_surprised_icon_24dp);
+                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonSurprised));
                 break;
         }
     }
