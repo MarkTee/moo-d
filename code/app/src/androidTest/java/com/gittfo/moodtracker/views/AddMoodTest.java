32c9dec61a4e1f7ca4530b5f1fddad9fcc37c58d
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-11-07 23:29:25 -0700

    add and delete mood test

--- code/app/src/androidTest/java/com/gittfo/moodtracker/views/AddMoodTest.java
+++ code/app/src/androidTest/java/com/gittfo/moodtracker/views/AddMoodTest.java
@@ -1,25 +1,32 @@
 package com.gittfo.moodtracker.views;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
-import com.gittfo.moodtracker.views.addmood.AddMoodEventActivity;
+import com.gittfo.moodtracker.database.Database;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.testng.reporters.jq.Main;
 
+import static android.content.Context.MODE_PRIVATE;
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
@@ -28,6 +35,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
@@ -35,11 +43,24 @@ import static org.hamcrest.Matchers.is;
 @RunWith(AndroidJUnit4.class)
 public class AddMoodTest {
 
+
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class, true, true);
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class, true, true){
+
+        @Override
+        protected void beforeActivityLaunched(){
+            InstrumentationRegistry.getInstrumentation().getTargetContext().getSharedPreferences(Database.PREFS, MODE_PRIVATE)
+                    .edit()
+                    .putString("user", "105648403813593449833")
+                    .apply();
+
+
+        }
+    };
 
     @Test
-    public void addMoodTest() {
+    public void addDeleteMoodTest() {
+
 
         ViewInteraction floatingActionButton = onView(
                 allOf(withId(R.id.fab),
@@ -132,6 +153,45 @@ public class AddMoodTest {
                                         0),
                                 14)));
         materialButton8.perform(scrollTo(), click());
+
+        ViewInteraction delete_button = onView(
+                allOf(withId(R.id.delete_button), withContentDescription("delete button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                6),
+                        isDisplayed()));
+        delete_button.perform(click());
+
+        ViewInteraction cancel_delete = onView(
+                allOf(withId(android.R.id.button2), withText("Cancel"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.buttonPanel),
+                                        0),
+                                2)));
+        cancel_delete.perform(scrollTo(), click());
+
+        ViewInteraction deleteButton = onView(
+                allOf(withId(R.id.delete_button), withContentDescription("delete button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                6),
+                        isDisplayed()));
+        deleteButton.perform(click());
+
+        ViewInteraction confirmDelete = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.buttonPanel),
+                                        0),
+                                3)));
+        confirmDelete.perform(scrollTo(), click());
+
     }
 
     private static Matcher<View> childAtPosition(
