0bd2e8fdf432b5cb3e38f4033c457564f8f407cb
arostron <alex.rostron24@gmail.com>
2019-11-07 22:39:59 -0700

    Kill MoodHistory but dont delete the file

--- code/app/src/main/java/com/gittfo/moodtracker/mood/MoodHistory.java
+++ code/app/src/main/java/com/gittfo/moodtracker/mood/MoodHistory.java
@@ -12,67 +12,68 @@ import java.util.ArrayList;
  * Also has the ability to render itself for viewing.
  */
 public class MoodHistory {
+//
+//    // The list of moods in this history
+//    private ArrayList<MoodEvent> moodEvents;
+//
+//
+//    /**
+//     * Create a new mood history with a given list of moods.
+//     *
+//     * @param moodEvents The list of moods to be held in this history.
+//     */
+//    public MoodHistory(ArrayList<MoodEvent> moodEvents) {
+//        this.moodEvents = moodEvents;
+//    }
+//
+//    /**
+//     * Create a new, empty mood history.
+//     */
+//    public MoodHistory() {
+//        this(new ArrayList<>());
+//    }
+//
+//    /**
+//     * Returns an ArrayList containing the MoodEvents in this MoodHistory
+//     *
+//     * @return The list of mood events in this history.
+//     */
+//    public ArrayList<MoodEvent> getMoodEvents() {
+//        return moodEvents;
+//    }
+//
+//    /**
+//     * Adds a MoodEvent to this MoodHistory
+//     *
+//     * @param moodEvent The mood event to add to this history.
+//     */
+//    public void addMoodEvent(MoodEvent moodEvent) {
+//        moodEvents.add(0, moodEvent);
+//    }
+//
+//    /**
+//     * Removes all mood events from this MoodHistory
+//     */
+//    public void clear() {
+//        this.moodEvents.clear();
+//    }
+//
+//    /**
+//     * Removes a single MoodEvent from this MoodHistory
+//     *
+//     * @param moodEvent The mood event to remove from this history.
+//     */
+//    public void removeMoodEvent(MoodEvent moodEvent) {
+//        moodEvents.remove(moodEvent);
+//    }
+//
+//    /**
+//     * Expose ArrayAdapter.notifyDataSetChanged() call.
+//     * do nothing if MoodHistory hasnt been rendered yet
+//     */
+//    public void notifyDataSetChanged(){
+//        this.moodEvents.sort((o1, o2) -> o2.getDate().compareTo(o1.getDate()));
+//    }
 
-    // The list of moods in this history
-    private ArrayList<MoodEvent> moodEvents;
-
-
-    /**
-     * Create a new mood history with a given list of moods.
-     *
-     * @param moodEvents The list of moods to be held in this history.
-     */
-    public MoodHistory(ArrayList<MoodEvent> moodEvents) {
-        this.moodEvents = moodEvents;
-    }
-
-    /**
-     * Create a new, empty mood history.
-     */
-    public MoodHistory() {
-        this(new ArrayList<>());
-    }
-
-    /**
-     * Returns an ArrayList containing the MoodEvents in this MoodHistory
-     *
-     * @return The list of mood events in this history.
-     */
-    public ArrayList<MoodEvent> getMoodEvents() {
-        return moodEvents;
-    }
-
-    /**
-     * Adds a MoodEvent to this MoodHistory
-     *
-     * @param moodEvent The mood event to add to this history.
-     */
-    public void addMoodEvent(MoodEvent moodEvent) {
-        moodEvents.add(0, moodEvent);
-    }
-
-    /**
-     * Removes all mood events from this MoodHistory
-     */
-    public void clear() {
-        this.moodEvents.clear();
-    }
-
-    /**
-     * Removes a single MoodEvent from this MoodHistory
-     *
-     * @param moodEvent The mood event to remove from this history.
-     */
-    public void removeMoodEvent(MoodEvent moodEvent) {
-        moodEvents.remove(moodEvent);
-    }
-
-    /**
-     * Expose ArrayAdapter.notifyDataSetChanged() call.
-     * do nothing if MoodHistory hasnt been rendered yet
-     */
-    public void notifyDataSetChanged(){
-        this.moodEvents.sort((o1, o2) -> o2.getDate().compareTo(o1.getDate()));
-    }
     
 }
