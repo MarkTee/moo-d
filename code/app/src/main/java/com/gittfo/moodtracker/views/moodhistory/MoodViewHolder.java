8a233c8937d5a63be6e60f2ccfd605bd567a6ef4
Mark Thomas <MarkTee@users.noreply.github.com>
2019-11-07 19:12:36 -0700

    Simplify populating MoodEventContainers

--- code/app/src/main/java/com/gittfo/moodtracker/views/moodhistory/MoodViewHolder.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/moodhistory/MoodViewHolder.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.gittfo.moodtracker.mood.Mood;
 import com.gittfo.moodtracker.mood.MoodEvent;
 import com.gittfo.moodtracker.views.R;
 
@@ -42,10 +43,13 @@ public class MoodViewHolder extends RecyclerView.ViewHolder {
      */
     public void populateMoodEventContainer(MoodEvent moodEvent){
         this.moodEventID = moodEvent.getId();
+        Mood mood = Mood.moodFromEmotionalState(moodEvent.getMood());
+        int moodColor = mood.getColor();
 
-        // Add the MoodEvent's emotional state
+        // Add the MoodEvent's emotional state (with the appropriate colour)
         TextView eventMood = container.findViewById(R.id.user_mood_textView);
         eventMood.setText(moodEvent.getMood().toString());
+        eventMood.setTextColor(moodColor);
 
         // Add the MoodEvent's reason
         TextView eventReason = container.findViewById(R.id.user_reason_textView);
@@ -63,43 +67,10 @@ public class MoodViewHolder extends RecyclerView.ViewHolder {
         TextView eventSocialSituation = container.findViewById(R.id.num_people_textView);
         eventSocialSituation.setText(moodEvent.getSocialSituation().toString().toLowerCase());
 
-        // Add the MoodEvent's mood emoticon
-        ImageView eventIcon = container.findViewById(R.id.user_emotion_imageView);
-        TextView eventColor = container.findViewById(R.id.user_mood_textView);
+        // Add the MoodEvent's mood emoticon (with the appropriate colour)
         ImageView eventEmoticon = container.findViewById(R.id.user_emotion_imageView);
-
-        switch  (moodEvent.getMood()){
-            case SAD:
-                eventIcon.setImageResource(R.drawable.ic_sad_icon_24dp);
-                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonSad));
-                eventEmoticon.setColorFilter(ContextCompat.getColor(eventEmoticon.getContext(), R.color.colorEmoticonSad));
-                break;
-            case ANGRY:
-                eventIcon.setImageResource(R.drawable.ic_angry_icon_24dp);
-                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonAngry));
-                eventEmoticon.setColorFilter(ContextCompat.getColor(eventEmoticon.getContext(), R.color.colorEmoticonAngry));
-                break;
-            case HAPPY:
-                eventIcon.setImageResource(R.drawable.ic_happy_icon_24dp);
-                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonHappy));
-                eventEmoticon.setColorFilter(ContextCompat.getColor(eventEmoticon.getContext(), R.color.colorEmoticonHappy));
-                break;
-            case AFRAID:
-                eventIcon.setImageResource(R.drawable.ic_afraid_icon_24dp);
-                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonAfraid));
-                eventEmoticon.setColorFilter(ContextCompat.getColor(eventEmoticon.getContext(), R.color.colorEmoticonAfraid));
-                break;
-            case DISGUSTED:
-                eventIcon.setImageResource(R.drawable.ic_disgusted_icon_24dp);
-                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonDisgusted));
-                eventEmoticon.setColorFilter(ContextCompat.getColor(eventEmoticon.getContext(), R.color.colorEmoticonDisgusted));
-                break;
-            case SURPRISED:
-                eventIcon.setImageResource(R.drawable.ic_surprised_icon_24dp);
-                eventColor.setTextColor(ContextCompat.getColor(eventColor.getContext(), R.color.colorEmoticonSurprised));
-                eventEmoticon.setColorFilter(ContextCompat.getColor(eventEmoticon.getContext(), R.color.colorEmoticonSurprised));
-                break;
-        }
+        eventEmoticon.setImageResource(mood.getEmoticon());
+        eventEmoticon.setColorFilter(mood.getColor());
     }
 
 
