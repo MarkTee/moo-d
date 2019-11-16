26bba069dbf780b15b52c6598685b78a47788d4e
Colter Scott MacDonald <colter1@ualberta.ca>
2019-11-16 10:19:21 -0700

    Comments, javadoc.

--- code/app/src/main/java/com/gittfo/moodtracker/views/map/MapActivity.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/map/MapActivity.java
@@ -1,18 +1,12 @@
 package com.gittfo.moodtracker.views.map;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import androidx.fragment.app.FragmentActivity;
 
 import com.gittfo.moodtracker.mood.MoodEvent;
 import com.gittfo.moodtracker.views.R;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -27,19 +21,29 @@ import java.util.ArrayList;
  */
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    // tag for passing in a mood history wrapper through intent extras
     public static final String MOOD_HISTORY_WRAPPER = "MOOD_HISTORY_WRAPPER";
+    // list of mood events to show on the map. source-agnostic, so we can use this for a personal
+    // history, or a friend's history, or whatever
     private ArrayList<MoodEvent> moodEvents;
 
+    /**
+     * Create a new map activity, set the view to the map view and call for a new map to populate
+     * it. If we were passed a MoodHistoryWrapper, unwrap it and save the history for displaying.
+     *
+     * @param savedInstance Reference to the Bundle object passed into the activity
+     */
     @Override
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_map_test);
 
-
+        // we use fragments for now, not a full on map view
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
             .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        // get the mood history
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             MoodHistoryWrapper wrapper = bundle.getParcelable(MOOD_HISTORY_WRAPPER);
@@ -47,8 +51,15 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         }
     }
 
+    /**
+     * When "getMapAsync" returns a GoogleMap, populate it with our MoodHistory locations.
+     *
+     * @param googleMap the Map to display mood events on.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        // for aesthetics, move the camera to the most recent mood event
         LatLng last = null;
         for (MoodEvent me : moodEvents) {
             LatLng moodLocation = new LatLng(me.getLatitude(), me.getLongitude());
