a1036edce0201cf6a214f9c07eead9e1063f0aad
arostron <alex.rostron24@gmail.com>
2019-11-18 12:53:20 -0700

    Add interface to view/edit username in database

--- code/app/src/main/java/com/gittfo/moodtracker/views/addmood/ProfileActivity.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/addmood/ProfileActivity.java
@@ -1,15 +1,41 @@
 package com.gittfo.moodtracker.views.addmood;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.gittfo.moodtracker.database.Database;
 import com.gittfo.moodtracker.views.R;
 
 /**
  * An activity for managing a user's profile.
  */
 public class ProfileActivity extends AppCompatActivity {
+    private EditText usernameView;
+    private Button updateButton;
+    private Context c;
 
-    protected void onCreate(){
-        setContentView(R.layout.user_inbox);  // no profile view yet, just default to this for now
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.placeholder_username); // Use a poorly made placeholder XML file
+
+        usernameView = findViewById(R.id.username);
+        updateButton = findViewById(R.id.update_username_button);
+        c = this;
+
+        String  username = Database.get(this).getUserName();
+        usernameView.setText(Database.get(this).getUserName());
+
+    }
+
+    public void updateUsername(View view){
+        Database.get(this).setUserName(usernameView.getText().toString());
     }
 }
