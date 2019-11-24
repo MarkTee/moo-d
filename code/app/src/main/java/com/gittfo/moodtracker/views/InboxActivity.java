6655f2714abbe3657fe1c4e40078d255f3ec5c82
Mark Thomas <MarkTee@users.noreply.github.com>
2019-11-24 15:29:07 -0700

    Wire up BottomAppBar for InboxActivity (except for Maps)

--- code/app/src/main/java/com/gittfo/moodtracker/views/InboxActivity.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/InboxActivity.java
@@ -1,6 +1,8 @@
 package com.gittfo.moodtracker.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,5 +17,33 @@ public class InboxActivity extends AppCompatActivity {
 
         // Hide the ActionBar
         getSupportActionBar().hide();
+    }
+
+    /**
+     * When the "inbox" button is pressed, don't do anything (since we're already in InboxActivity)
+     * @param view the Inbox button.
+     */
+    public void startTimelineActivity(View view) {
+        // don't animate transition between activities
+        Intent i = new Intent(this, TimelineActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        this.startActivity(i);
+    }
+
+    /**
+     * When the "inbox" button is pressed, go to the inbox activity.
+     * @param view the Inbox button.
+     */
+    public void startInboxActivity(View view) {
+        ;
+    }
+
+    /**
+     * When the "profile" button is pressed, don't do anything (since we're already in MainActivity)
+     * @param view the Inbox button.
+     */
+    public void startProfileActivity(View view){
+        // don't animate transition between activities
+        Intent i = new Intent(this, ProfileActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        this.startActivity(i);
     }
 }
