386a07d9798b4336c5de4b5025895061e084a453
Mark Thomas <MarkTee@users.noreply.github.com>
2019-11-24 15:26:39 -0700

    Wire up BottomAppBar for TimelineActivity (except for Maps)

--- code/app/src/main/java/com/gittfo/moodtracker/views/TimelineActivity.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/TimelineActivity.java
@@ -1,6 +1,8 @@
 package com.gittfo.moodtracker.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,5 +17,32 @@ public class TimelineActivity extends AppCompatActivity {
 
         // Hide the ActionBar
         getSupportActionBar().hide();
+    }
+
+    /**
+     * When the "timeline" button is pressed, don't do anything (since we're already in TimelineActivity)
+     * @param view the Inbox button.
+     */
+    public void startTimelineActivity(View view) {
+        ;
+    }
+
+    /**
+     * When the "inbox" button is pressed, go to the inbox activity.
+     * @param view the Inbox button.
+     */
+    public void startInboxActivity(View view) {
+        // don't animate transition between activities
+        Intent i = new Intent(this, InboxActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        this.startActivity(i);
+    }
+
+    /**
+     * When the "profile" button is pressed, go to the profile activity.
+     * @param view the Profile button.
+     */
+    public void startProfileActivity(View view){
+        Intent i = new Intent(this, ProfileActivity.class);
+        this.startActivity(i);
     }
 }
