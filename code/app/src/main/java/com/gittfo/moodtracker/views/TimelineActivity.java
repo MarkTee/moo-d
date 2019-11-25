67a3bcc0ea4e10aacee75fe585b25d8129fbbd61
Chuck H <cthanson@ualberta.ca>
2019-11-24 19:27:49 -0700

    Gave Timeline a working dropdown menu

--- code/app/src/main/java/com/gittfo/moodtracker/views/TimelineActivity.java
+++ code/app/src/main/java/com/gittfo/moodtracker/views/TimelineActivity.java
@@ -2,7 +2,11 @@ package com.gittfo.moodtracker.views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
  * Contains the most recent MoodEvents of a user's followed users
  */
 public class TimelineActivity extends AppCompatActivity {
+
+    private ImageButton dropDownButton;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -53,5 +59,56 @@ public class TimelineActivity extends AppCompatActivity {
         // don't animate transition between activities
         Intent i = new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         this.startActivity(i);
+    }
+
+    public void startSigninActivity(View view){
+        Intent i = new Intent(this, SigninActivity.class);
+        this.startActivity(i);
+    }
+
+    public void startUsernameActivity(View view){
+        Intent i = new Intent(this, ChangeUsernameActivity.class);
+        this.startActivity(i);
+    }
+
+    public void dropdownPressed(View view){
+        dropDownButton = (ImageButton) findViewById(R.id.settings_button);
+        dropDownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // create a PopupMenu
+                PopupMenu popup = new PopupMenu(TimelineActivity.this, dropDownButton);
+                // inflate the popup via xml file
+                popup.getMenuInflater()
+                        .inflate(R.menu.dropdown_menu, popup.getMenu());
+
+                // tie popup to OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch(menuItem.getItemId()) {
+                            case (R.id.dropdown_one):
+                                // change username
+                                startUsernameActivity(view);
+                                break;
+                            case (R.id.dropdown_two):
+                                // change color scheme
+                                //TODO: color scheme change functionality
+                                Toast.makeText(TimelineActivity.this,
+                                        "Coming soon!",
+                                        Toast.LENGTH_SHORT).show();
+                                break;
+                            case (R.id.dropdown_three):
+                                //TODO: allow for proper log out?
+                                startSigninActivity(view);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popup.show(); // show popup menu
+            }
+        }); // close setOnClickListener method
+
     }
 }
