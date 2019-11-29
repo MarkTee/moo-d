81e8573f620453064db37102bf24bb9848fd48c1
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-11-28 21:45:10 -0700

    signouttest

--- code/app/src/androidTest/java/com/gittfo/moodtracker/views/SignOutTest.java
+++ code/app/src/androidTest/java/com/gittfo/moodtracker/views/SignOutTest.java
@@ -1,11 +1,6 @@
 package com.gittfo.moodtracker.views;
 
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -13,9 +8,6 @@ import androidx.test.runner.AndroidJUnit4;
 
 import com.gittfo.moodtracker.database.Database;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,12 +16,9 @@ import static android.content.Context.MODE_PRIVATE;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -49,44 +38,10 @@ public class SignOutTest {
 
     @Test
     public void signOutTest() {
+        onView(withId(R.id.settings_button)).perform(click());
+        onView(withText("Sign out")).perform(click());
 
-        ViewInteraction appCompatImageButton = onView(
-                allOf(withId(R.id.settings_button), withContentDescription("menu button"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.profile_toolbar),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatImageButton.perform(click());
-
-        ViewInteraction materialTextView = onView(
-                allOf(withId(android.R.id.title), withText("Sign out"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        materialTextView.perform(click());
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        onView(allOf(withText("moo-d"), isDisplayed()));
+        onView(allOf(withId(R.id.sign_in_button), withText("Sign In"), isDisplayed()));
     }
 }
